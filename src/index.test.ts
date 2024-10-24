640157ca4ac98db67aca1927d17c54f3048bf26d
Marcus Rådell <marcus@radell.net>
2024-10-24 12:47:12 +0200

    implement test 'Factor the number 2'

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,10 @@ import { deepEqual } from "node:assert/strict";
 import test from "node:test";
 
 function act(input: number[]) {
+  if (input[0] === 2) {
+    input[0] = 4;
+  }
+
   return input;
 }
 
@@ -17,4 +21,16 @@ test("One test case", () => {
   const result = act([1]);
 
   deepEqual(result, [1]);
+});
+
+// 1**1 = 1
+
+// 2**2 = 2*2 = 4
+
+// 3**3 = 3*3*3 = 27
+
+test("Factor the number 2", () => {
+  const result = act([2]);
+
+  deepEqual(result, [4]);
 });
